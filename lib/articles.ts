42c489d115f380cc22353f6d3cfaa33b9b8229a9
SeanXLChen <xiaolaiseanchen@gmail.com>
2024-08-01 10:41:45 -0700

    Define backend code to read articles into object

---
+++ lib/articles.ts
@@ -0,0 +1,60 @@
+// The backend code to fetch articles from the database
+import fs from 'fs';
+import matter from 'gray-matter';
+import path from 'path';
+import moment from 'moment';
+import { remark } from 'remark';
+import html from 'remark-html';
+
+import type { ArticleItem } from '@/type';
+
+// The directory where the articles are stored
+const articlesDirectory = path.join(process.cwd(), 'articles');
+
+const getSortedArticles = (): ArticleItem[] => {
+    const fileNames: string[] = fs.readdirSync(articlesDirectory); // Get all the file names in the articles directory
+
+    const allArticleData = fileNames.map((fileName) => {
+        const id: string = fileName.replace(/\.md$/, ''); // Remove the .md extension
+        const fullPath = path.join(articlesDirectory, fileName); // Get the full path of the file
+        const fileContents = fs.readFileSync(fullPath, 'utf-8'); // Read the file contents
+        const matterResult = matter(fileContents); // store the mata data of the article
+
+        return {
+            id: id,
+            title: matterResult.data.title,
+            date: matterResult.data.date,
+            category: matterResult.data.category,
+        };
+    });
+
+    return allArticleData.sort((a, b) => {
+        const format = "DD-MM-YYYY";
+        const dateA = moment(a.date, format);
+        const dateB = moment(b.date, format);
+        if (dateA.isBefore(dateB)) {
+            return -1;
+        }
+        else if (dateA.isAfter(dateB)) {
+            return 1;
+        }
+        else {
+            return 0;
+        }
+    });
+}
+
+export const getCategorisedArticles = (): Record<string, ArticleItem[]> => {
+    const sortedArticles = getSortedArticles();
+    const categorisedArticles: Record<string, ArticleItem[]> = {};
+
+    sortedArticles.forEach((article) => {
+        if (!categorisedArticles[article.category]) {
+            categorisedArticles[article.category] = [];
+        } 
+        categorisedArticles[article.category].push(article);
+    });
+
+    return categorisedArticles;
+}
+
